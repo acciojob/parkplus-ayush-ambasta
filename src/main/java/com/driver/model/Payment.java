bbc8ce6e34a6a6beb5ddd756a7278adb8eaa78be
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-03-04 15:22:58 +0530

    solution 14

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -15,6 +15,15 @@ public class Payment {
     boolean paymentCompleted;
     PaymentMode paymentMode;
 
+    public Payment(Integer id, boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
+        this.id = id;
+        this.paymentCompleted = paymentCompleted;
+        this.paymentMode = paymentMode;
+        this.reservation = reservation;
+    }
+
+    public Payment() {
+    }
     public int getId() {
         return id;
     }
--- src/main/java/com/driver/model/Spot.java
+++ src/main/java/com/driver/model/Spot.java
@@ -1,6 +1,7 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,7 +14,7 @@ public class Spot {
     private ParkingLot parkingLot;
 
     @OneToMany(mappedBy = "spot",cascade = CascadeType.ALL)
-    List<Reservation> reservationList;
+    List<Reservation> reservationList=new ArrayList<>();
 
     SpotType spotType;
     private Integer pricePerHour;
